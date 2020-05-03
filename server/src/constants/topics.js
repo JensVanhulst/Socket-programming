f3403a4fcb6a625bf9b0067786459f76e32dc855
Jens Vanhulst <vanhulstjens@gmail.com>
2020-05-03 17:34:51 +0200

    add basic tournament class and removeDS_Store

---
+++ server/src/constants/topics.js
@@ -0,0 +1,5 @@
+/** @format */
+
+module.exports = {
+  JOIN: 'conn4>tournament>name>join>',
+};
--- server/src/index.js
+++ server/src/index.js
@@ -1,27 +1,28 @@
 /** @format */
 require('dotenv').config();
-
 const zmq = require('zeromq');
-const responder = zmq.socket('rep');
+const Tournament = require('./classes/Tournament');
+const topics = require('./constants/topics');
 
-const PORT = process.env.PORT || 9000;
+const socket = new zmq.Subscriber();
+const tournament = new Tournament(4);
 
-responder.on('message', (request) => {
-  console.log(`Received request: [ ${request.toString()}]`);
-
-  setTimeout(() => {
-    responder.send('Hi there');
-  }, 1000);
-});
-
-responder.bind('tcp://*:' + PORT, (err) => {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log(`Listening on ${PORT}`);
+const run = async () => {
+  try {
+    await socket.connect('tcp://193.190.154.184:24042');
+    await socket.subscribe('');
+    console.log('[INFO] - Connected and subscibed to all topics !');
+    console.log(`Created a new tournament with id ${tournament.id}`);
+  } catch (e) {
+    console.log(e);
   }
-});
 
-process.on('SIGINT', () => {
-  responder.close();
-});
+  console.log(tournament.gPlayerAmount);
+
+  for await (const [topic, msg] of socket) {
+    tournament.addPlayer(String(msg));
+    console.log(tournament.gPlayerAmount);
+  }
+};
+
+run();
