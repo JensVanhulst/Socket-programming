a0157983c509b304d01599891db0e4c5fae74ed1
Jens Vanhulst <vanhulstjens@gmail.com>
2020-05-26 02:46:54 +0200

    update for electron builder

--- client/vue.config.js
+++ client/vue.config.js
@@ -2,4 +2,17 @@ module.exports = {
   transpileDependencies: [
     'vuetify',
   ],
+  pluginOptions: {
+    electronBuilder: {
+      builderOptions: {
+        // options placed here will be merged with default configuration and passed to electron-builder
+
+        appId: 'PXL-Socket-Programming',
+        mac: {
+          target: 'pkg',
+        },
+
+      },
+    },
+  },
 };
